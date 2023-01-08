9826a91266e93cb694f26fd1da18847e18082f50
MarcoGorelli <>
2023-01-08 18:33:18 +0000

    try with kaggle api

--- download_file.py
+++ download_file.py
@@ -1,5 +1,13 @@
 import gdown 
+import kaggle
 
-url = 'https://drive.google.com/uc?id=16Y8SSNqMDGyIhedgF6BzASSXtKZIL89u'
-output = 'output/pandas-dev/.coverage'
-gdown.download(url, output)
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+api = KaggleApi()
+api.authenticate()
+
+api.kernels_output('marcogorelli/who-tests-what-in-pandas', 'output')
+
+#url = 'https://drive.google.com/uc?id=16Y8SSNqMDGyIhedgF6BzASSXtKZIL89u'
+#output = 'output/pandas-dev/.coverage'
+#gdown.download(url, output)
