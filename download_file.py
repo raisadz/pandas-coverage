41c74def76f828e94b4c49e3022ea0116807d5ef
MarcoGorelli <>
2023-01-08 17:02:32 +0000

    update database

--- download_file.py
+++ download_file.py
@@ -1,5 +1,5 @@
 import gdown 
 
-url = 'https://drive.google.com/uc?id=1V9ZJI_t9-Genc7nBlQWBqFi_a_aCuCrB'
+url = 'https://drive.google.com/uc?id=16Y8SSNqMDGyIhedgF6BzASSXtKZIL89u'
 output = 'output/pandas-dev/.coverage'
 gdown.download(url, output)
