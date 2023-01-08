cfa6b6940da674a57193b55de0056d66df6800b6
MarcoGorelli <>
2023-01-08 16:39:27 +0000

    new coverage file

--- download_file.py
+++ download_file.py
@@ -1,5 +1,5 @@
 import gdown 
 
-url = 'https://drive.google.com/uc?id=1RqLvO0NA5yutlXreo8MdoewQdyVGi77h'
+url = 'https://drive.google.com/uc?id=1V9ZJI_t9-Genc7nBlQWBqFi_a_aCuCrB'
 output = 'output/pandas-dev/.coverage'
 gdown.download(url, output)
