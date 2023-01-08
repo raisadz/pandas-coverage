5ea0a0d89ae4b88aa151cc1e035e3232ab3a5575
Raisadz <rdzhamtyrova@protonmail.com>
2023-01-08 14:44:08 +0000

    just depth 10

--- clone_pandas.py
+++ clone_pandas.py
@@ -6,5 +6,5 @@ output_files = os.listdir('output')
 pattern_commit = re.compile(r'coverage_(.+?)\.db')
 pandas_commit = [pattern_commit.search(i).group(1) for i in output_files if pattern_commit.search(i)][0]
 
-subprocess.run(['git', 'clone', 'https://github.com/pandas-dev/pandas.git'])
+subprocess.run(['git', 'clone', 'https://github.com/pandas-dev/pandas.git', '--depth', 10])
 subprocess.run(['git', 'checkout', pandas_commit], cwd='pandas')
