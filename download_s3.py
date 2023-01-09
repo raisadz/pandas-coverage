2c8db272bd813d37bce1a807323684c2485d5373
Raisadz <rdzhamtyrova@protonmail.com>
2023-01-09 15:50:46 +0000

    add boto3 file

---
+++ download_s3.py
@@ -0,0 +1,10 @@
+import boto3
+
+client = boto3.client('s3')
+s3 = boto3.resource('s3')
+
+bucket = s3.Bucket('pandas-coverage')
+
+myfiles = list(bucket.objects.all())
+for file in myfiles:
+    client.download_file('pandas-coverage', file.key, file.key)
