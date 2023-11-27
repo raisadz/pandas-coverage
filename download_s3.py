fe106fb1c305ea1eac8fe8eb4688c035728d00a1
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-11-27 15:31:04 +0000

    update

--- download_s3.py
+++ download_s3.py
@@ -3,8 +3,12 @@ download data from aws s3 bucket pandas-coverage
 """
 
 import gdown
+import re
 
 url = 'https://drive.google.com/uc?export=download&id=1w12bgXVSVMzCui2EOGVuuXSZ5Ql-Nk82'
+id_ = re.search(r'download&id=(.*)', url).group(1)
+url = f'https://drive.google.com/file/d/{id_}/view?usp=sharing'
+print('url', url)
 output = 'coverage.db'
 gdown.download(url, output, quiet=False)
 
