2f84247ac874edb3077373b20b864caa47740566
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-05 15:37:38 +0000

    update

--- download_s3.py
+++ download_s3.py
@@ -6,7 +6,7 @@ import gdown
 import re
 
 # paste the raw url below
-url = 'https://drive.google.com/file/d/1DZ0IsX7fSlS_bHUJVdxKuuWLgKVsakT5/view?usp=sharing'
+url = 'https://drive.google.com/file/d/1nZ2-ZYk_IWBHDBHtBtOpseaQr05B8zIA/view?usp=sharing'
 id_ = re.search(r'file/d/(.*)/view?', url).group(1)
 url = f'https://drive.google.com/uc?export=download&id={id_}'
 print('url', url)
--- how-to-update-manually.md
+++ how-to-update-manually.md
@@ -2,6 +2,6 @@
 2. record the commit used in metadata.txt
 3. download the coverage file, and upload to google drive
 4. share the google drive file, select "anyone with link", and copy the link
-5. get the id from the link, and put it in `download_s3.py` file
+5. get the link, and paste it in `download_s3.py` file
 6. commit and push
 7. in heroku, redeploy app
